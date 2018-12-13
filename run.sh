96ac67ffc29e289c2c43252a509707ba53f77c42
Samuel Bernard <sb@make.org>
2018-12-13 19:05:24 +0100

    fix: set properties via command line instead

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-echo 'include=secor.prod.partition.properties' > secor.current.properties
-# get all env, filter secor config and output key=value in config file
+properties=
+# get all env, filter secor config and append -Dkey=value in properties var
 for config in $(env | grep '^SECOR'); do
   var=$(echo $config | cut -f1 -d'=')
   value=$(echo $config | cut -f2 -d'=')
   # convert SECOR_property_complete__name to property.complete_name
   key=$(echo $var | sed 's/^SECOR_//g' | tr '_' '.' | sed 's/\.\./_/g')
-  echo "$key=$value" >> secor.current.properties
+  properties="$properties -D$key=$value"
 done
 
 java -Xmx${JVM_MEMORY:-512m} -ea \
   -Dlog4j.configuration=log4j.docker.properties \
-  -Dconfig=secor.current.properties \
+  -Dconfig=secor.prod.partition.properties \
+  $properties \
   -cp secor.jar:lib/* \
   com.pinterest.secor.main.ConsumerMain
